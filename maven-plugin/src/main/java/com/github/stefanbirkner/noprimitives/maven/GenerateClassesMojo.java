5f45386d65839bca31bc2a3c476bd6225e61753c
Stefan Birkner <mail@stefan-birkner.de>
2012-06-14 09:43:16 +0200

    Automatic compilation of generated sources.

--- maven-plugin/src/main/java/com/github/stefanbirkner/noprimitives/maven/GenerateClassesMojo.java
+++ maven-plugin/src/main/java/com/github/stefanbirkner/noprimitives/maven/GenerateClassesMojo.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 
 import com.github.stefanbirkner.noprimitives.generator.CodeGenerator;
 import com.github.stefanbirkner.noprimitives.generator.Request;
@@ -22,30 +23,36 @@ import com.github.stefanbirkner.noprimitives.generator.Request;
 public class GenerateClassesMojo extends AbstractMojo {
     private static final ConfigurationReader CONFIGURATION_READER = new ConfigurationReader();
     private static final CodeGenerator CODE_GENERATOR = new CodeGenerator();
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+    
+    /**
+     * Location of the output files.
+     * 
+     * @parameter default-value= "${project.build.directory}/generated-sources/no-primitives-maven-plugin/"
+     * @required
+     */
+    private File outputDirectory;
 
-	/**
-	 * Location of the output files.
-	 * 
-	 * @parameter default-value=
-	 *            "${project.build.directory}/generated-sources/no-primitives/"
-	 * @required
-	 */
-	private File outputDirectory;
+    /**
+     * Location of the configuration files.
+     * 
+     * @parameter default-value="src/main/no-primitives/"
+     * @required
+     */
+    private File configurationDirectory;
 
-	/**
-	 * Location of the configuration files.
-	 * 
-	 * @parameter default-value="src/main/no-primitives/"
-	 * @required
-	 */
-	private File configurationDirectory;
-
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
         Collection<Request> requests = requestsFromConfigurationFiles();
         SourceCodeFileGenerator generator = new SourceCodeFileGenerator(outputDirectory, CODE_GENERATOR);
         generator.createSourceCodeFilesForRequests(requests);
-	}
+        project.addCompileSourceRoot(outputDirectory.getAbsolutePath()); 
+    }
 
     private Collection<Request> requestsFromConfigurationFiles() {
         Set<Request> requests = new HashSet<Request>();

80122dad68ff6272e8b1b3b4a1acda31ff3834e8
Stefan Birkner <mail@stefan-birkner.de>
2012-07-11 20:44:16 +0200

    Use Java5 annotations to generate the plugin descriptor file.

--- maven-plugin/src/main/java/com/github/stefanbirkner/semanticwrapper/maven/GenerateClassesMojo.java
+++ maven-plugin/src/main/java/com/github/stefanbirkner/semanticwrapper/maven/GenerateClassesMojo.java
@@ -1,49 +1,43 @@
 package com.github.stefanbirkner.semanticwrapper.maven;
 
+import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_SOURCES;
+
 import java.io.File;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
 import com.github.stefanbirkner.semanticwrapper.generator.CodeGenerator;
 import com.github.stefanbirkner.semanticwrapper.generator.Request;
 
 /**
  * Create classes using the Semantic Wrapper code generator.
- * 
- * @goal generate
- * @phase generate-sources
- * @requiresProject true
  */
+@Mojo(name = "generate", defaultPhase = GENERATE_SOURCES, requiresProject = true)
 public class GenerateClassesMojo extends AbstractMojo {
     private static final ConfigurationReader CONFIGURATION_READER = new ConfigurationReader();
     private static final CodeGenerator CODE_GENERATOR = new CodeGenerator();
 
-    /**
-     * @parameter default-value="${project}"
-     * @required
-     * @readonly
-     */
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
     /**
      * Location of the output files.
-     * 
-     * @parameter default-value=
-     *            "${project.build.directory}/generated-sources/semantic-wrapper-maven-plugin/"
-     * @required
      */
+    @Parameter(defaultValue = "${project.build.directory}/generated-sources/semantic-wrapper-maven-plugin/", required = true)
     private File outputDirectory;
 
     /**
      * Location of the configuration files.
-     * 
-     * @parameter default-value="src/main/semantic-wrapper/"
-     * @required
      */
+    @Parameter(defaultValue = "src/main/semantic-wrapper/", required = true)
     private File configurationDirectory;
 
     @Override
@@ -60,5 +54,4 @@ public class GenerateClassesMojo extends AbstractMojo {
             requests.addAll(CONFIGURATION_READER.requestsFromConfigurationFile(configurationFile));
         return requests;
     }
-
 }
